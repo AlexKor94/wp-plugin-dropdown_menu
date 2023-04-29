bcc63d66e54ed9ac2c78d296c5c6e27da8e32944
AlexKor94 <sasha1994work@gmail.com>
2023-04-29 13:33:41 +0300

    create index php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       Dropdown menu
+ * Description:       Dropdown menu for my theme.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            AlexKor94
+ * Text Domain:       sub-menu
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
