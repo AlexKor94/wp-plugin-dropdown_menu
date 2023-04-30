c13de963c0b51dda1a144c356fd6033f5a76c67e
AlexKor94 <sasha1994work@gmail.com>
2023-04-30 23:36:54 +0300

    success get all pages

--- src/index.js
+++ src/index.js
@@ -3,25 +3,33 @@ import block from "./block.json";
 import { __ } from '@wordpress/i18n';
 import { InspectorControls, RichText, List } from '@wordpress/block-editor';
 import { PanelBody, TextControl, SelectControl, Button } from "@wordpress/components";
+import { useSelect } from '@wordpress/data';
+import { store as coreDataStore } from '@wordpress/core-data';
+import { decodeEntities } from '@wordpress/html-entities';
 
 registerBlockType(block.name, {
   edit({ attributes, setAttributes }) {
+    function PagesList({ pages }) {
+      return (
+        <ul>
+          {pages?.map(page => (
+            <li key={page.id}>
+              {decodeEntities(page.title.rendered)}
+            </li>
+          ))}
+        </ul>
+      )
+    }
+
+    const pages = useSelect(
+      select =>
+        select(coreDataStore).getEntityRecords('postType', 'page'),
+      []
+    );
+
     const { title, links, type } = attributes;
     return (
-      <>
-        <TextControl
-          label={__('Navigation Title', 'sub-menu')}
-          value={title}
-          onChange={(value) => setAttributes({ title: value })}
-        />
-        <List
-        />
-      </>
+      <PagesList pages={pages} />
     );
-  },
-  save({ attributes }) {
-    const { title, links, type } = attributes;
-    <List
-    />
-  },
+  }
 });
