f90487406fc64f2544f50a94fd079acd9207976f
AlexKor94 <sasha1994work@gmail.com>
2023-04-30 22:17:10 +0300

    worked example code with list ul

--- src/index.js
+++ src/index.js
@@ -1,21 +1,65 @@
 import { registerBlockType } from '@wordpress/blocks';
 import block from "./block.json";
 import { __ } from '@wordpress/i18n';
-import { InspectorControls, RichText } from '@wordpress/block-editor';
-import { PanelBody, TextControl } from "@wordpress/components";
+import { InspectorControls, RichText, List } from '@wordpress/block-editor';
+import { PanelBody, TextControl, SelectControl } from "@wordpress/components";
 
 registerBlockType(block.name, {
   edit({ attributes, setAttributes }) {
-    return (
-      <>
-        <RichText tagName='nav' placeholder={__('Enter text...', 'sub-menu')} />
-      </>
-    );
-  },
-  save({ attributes }) {
+    const { title, links, type } = attributes;
     return (
       <div>
+        <TextControl
+          label={__('Navigation Title')}
+          value={title}
+          onChange={(value) => setAttributes({ title: value })}
+        />
+        <SelectControl
+          label={__('Navigation Type')}
+          options={[
+            { label: __('Unordered List'), value: 'unordered' },
+            { label: __('Ordered List'), value: 'ordered' },
+          ]}
+          value={type}
+          onChange={(value) => setAttributes({ type: value })}
+        />
+        <ul>
+          {links.map((link, index) => (
+            <li key={index}>
+              <TextControl
+                value={link}
+                onChange={(value) => {
+                  const newLinks = [...links];
+                  newLinks[index] = value;
+                  setAttributes({ links: newLinks });
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => setAttributes({ links: [...links, ''] })}>
+          {__('Add Link')}
+        </button>
       </div>
     );
   },
+  save({ attributes }) {
+    const { title, links, type } = attributes;
+    const List = type === 'unordered' ? 'ul' : 'ol';
+    return (
+      <div>
+        {title && <h2>{title}</h2>}
+        <nav>
+          <List>
+            {links.map((link, index) => (
+              <li key={index}>
+                <a href={link}>{link}</a>
+              </li>
+            ))}
+          </List>
+        </nav>
+      </div>
+    );
+  },
+
 });
