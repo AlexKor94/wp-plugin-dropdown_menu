0cbc6a4548aa3677d44e37604bf0a1afdd27adab
AlexKor94 <sasha1994work@gmail.com>
2023-04-30 12:33:09 +0300

    work with PanelBody, TextControl

--- src/index.js
+++ src/index.js
@@ -5,19 +5,44 @@ import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, TextControl } from "@wordpress/components";
 
 registerBlockType(block.name, {
+  // title: __('Custom List'),
+  // icon: 'list-view',
+  // category: 'common',
+  // attributes: {
+  //   items: {
+  //     type: 'array',
+  //     source: 'children',
+  //     selector: 'ul',
+  //     default: []
+  //   }
+  // },
   edit({ attributes, setAttributes }) {
+    const onChangeItems = (value) => {
+      setAttributes({ items: value.split("\n") });
+    };
+
     return (
       <>
         <InspectorControls>
-          <PanelBody title={__('Custom Link', 'my-plugin')}>
+          <PanelBody title={__('Custom Link', block.name)}>
             <TextControl
-              label={__('Custom Link', 'my-plugin')}
+              label={__('Custom Link', block.name)}
               value={attributes.myCustomLink}
               onChange={(value) => setAttributes({ myCustomLink: value })}
             />
           </PanelBody>
         </InspectorControls>
-        <p>some content</p>
+        <ul>
+          {attributes.items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <TextControl
+          label={__('Add item', block.name)}
+          value=""
+          onChange={(event) => onChangeItems(event.target.value)}
+          placeholder={__('Type an item and press Enter', block.name)}
+        />
       </>
     );
   },
@@ -25,7 +50,11 @@ registerBlockType(block.name, {
   save({ attributes }) {
     return (
       <div>
-        <p>some content</p>
+        <ul>
+          {attributes.items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
     );
   },
