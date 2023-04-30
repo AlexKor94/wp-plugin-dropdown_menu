7dd3f64f02d1fd14d6d93c99a9fb25f38df04ca0
AlexKor94 <sasha1994work@gmail.com>
2023-04-30 23:14:40 +0300

    stay try

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import { registerBlockType } from '@wordpress/blocks';
 import block from "./block.json";
 import { __ } from '@wordpress/i18n';
-import { InspectorControls, RichText } from '@wordpress/block-editor';
-import { PanelBody, TextControl, SelectControl, Button, List } from "@wordpress/components";
+import { InspectorControls, RichText, List } from '@wordpress/block-editor';
+import { PanelBody, TextControl, SelectControl, Button } from "@wordpress/components";
 
 registerBlockType(block.name, {
   edit({ attributes, setAttributes }) {
     const { title, links, type } = attributes;
     return (
-      <div>
+      <>
         <TextControl
-          label={__('Navigation Title')}
+          label={__('Navigation Title', 'sub-menu')}
           value={title}
           onChange={(value) => setAttributes({ title: value })}
         />
-      </div>
+        <List
+        />
+      </>
     );
-  }
+  },
+  save({ attributes }) {
+    const { title, links, type } = attributes;
+    <List
+    />
+  },
 });
