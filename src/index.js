570186ef8fa933f83cf6643839414f5ceb042060
AlexKor94 <sasha1994work@gmail.com>
2023-05-02 19:58:03 +0300

    works a need structure in the editor

--- src/index.js
+++ src/index.js
@@ -11,46 +11,77 @@ import { decodeEntities } from '@wordpress/html-entities';
 registerBlockType(block.name, {
 
   edit({ attributes, setAttributes }) {
-    const { title, menu } = attributes;
+    const { title } = attributes;
+    const [menu, setMenu] = useState(attributes.menu);
 
-    const onAddItem = (arr) => {
-      const newMenu = [...menu, arr];
-      setAttributes({ menu: newMenu });
+    const handleMenuChange = (newMenu) => {
+      setMenu(newMenu);
     };
 
+    const handleSaveClick = () => {
+      setAttributes({ menu });
+    };
     return (
       <>
-        <TextControl label={__('Menu Title')} value={title} onChange={(title) => setAttributes({ title })} />
+        <TextControl
+          label={__("Menu Title")}
+          value={attributes.title}
+          onChange={(title) => setAttributes({ title })}
+        />
 
         <ul>
-          {
-            menu.map(element => {
-              return (
-                <li>
-                  <span className='opener'>
+          {menu.map((item, index) => (
+            <li key={index}>
+              <span className="opener">
+                <TextControl
+                  label={__("Item")}
+                  value={item.linkTitle}
+                  onChange={(linkTitle) => {
+                    const newMenu = [...menu];
+                    newMenu[index].linkTitle = linkTitle;
+                    handleMenuChange(newMenu);
+                  }}
+                />
+              </span>
+              <ul>
+                {item.subItems.map((subItem, subIndex) => (
+                  <li key={subIndex}>
                     <TextControl
-                      label={__('Item')}
-                      value={element.linkTitle} />
-                  </span>
-                  <ul>
-                    {element.subItems.map(el => {
-                      return (
-                        <li>
-                          <TextControl
-                            label={__('SubItem')}
-                            value={el.itemName} />
-                        </li>
-                      )
-                    })}
-                  </ul>
-                  <Button>{__('Add SubItem')}</Button>
-                </li>
-              );
-            })
-          }
+                      label={__("SubItem")}
+                      value={subItem.itemName}
+                      onChange={(itemName) => {
+                        const newMenu = [...menu];
+                        newMenu[index].subItems[subIndex].itemName = itemName;
+                        handleMenuChange(newMenu);
+                      }}
+                    />
+                  </li>
+                ))}
+              </ul>
+              <Button
+                onClick={() => {
+                  const newMenu = [...menu];
+                  newMenu[index].subItems.push({ itemName: "" });
+                  handleMenuChange(newMenu);
+                }}
+              >
+                {__("Add SubItem")}
+              </Button>
+            </li>
+          ))}
         </ul>
 
-        <Button>{__('Add Item')}</Button>
+        <Button
+          onClick={() => {
+            const newMenu = [...menu];
+            newMenu.push({ linkTitle: "", subItems: [{ itemName: "" }] });
+            handleMenuChange(newMenu);
+          }}
+        >
+          {__("Add Item")}
+        </Button>
+
+        <Button onClick={handleSaveClick}>{__("Save")}</Button>
       </>
     );
   }
